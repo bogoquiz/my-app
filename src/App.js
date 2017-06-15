f2ddcd7fe6129f76bdc0b6d8f6da356ac86d5a5c
Fredy B <tovar_fredy@hotmail.com>
2017-06-15 12:05:02 -0500

    list jsx

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const color = ["verdes","rojo","azul"]
     return (
+      <div>
         <h1>HOLA REACT!!!</h1>
+        <p>jelou</p>
+
+        <ul>
+          {color.map(name => 
+            <li key={name}>{name}</li>
+          )}
+        </ul>
+      </div>  
     );
   }
 }
