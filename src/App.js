d905e5aa1f781d15963709d0e4dc83e84d7cdb1a
Fredy B <tovar_fredy@hotmail.com>
2017-06-15 12:37:00 -0500

    component func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Hifive from './Title.js'
 import './App.css';
+
 
 class App extends Component {
   render() {
+    const techi = ["reactivo", "angulito", "backyni"]
     return (
-        <h1>HOLA REACT!!!</h1>
+      <div>
+        <Hifive name="nodecito" />
+        <Hifive name="webpackcito" />
+        {techi.map(frame => 
+          <Hifive name={frame} />)}
+      </div>  
     );
   }
 }
---
+++ src/Title.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hifive = (props) => <h1>HOLA REACT {props.name} !!! en componente</h1>
+
+export default Hifive
