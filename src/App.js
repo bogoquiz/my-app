ff22f7b9711ed93af8e6f7c43f961ff86d472d91
Fredy B <tovar_fredy@hotmail.com>
2017-06-16 17:44:53 -0500

    axio http

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+  	super();
+
+  	this.state = {
+  		names: []
+  	};
+
+  	axios.get("http://localhost:3001/names")
+  		 .then(response => {
+  		 	console.log(response)
+  		 	this.setState({
+  		 		names: response.data
+  		 	})
+  		 })
+  		 .catch(error => {
+  		 	console.log(error)
+  		 })
+  }	
   render() {
     return (
-        <h1>HOLA REACT!!!</h1>
+    	<div>
+        	<h1>HOLA http!!!</h1>
+        	<ul>
+        		{this.state.names.map(item =>
+        			<li key={item}>{item}</li>
+        		)}
+        	</ul>
+        </div>	
     );
   }
 }
