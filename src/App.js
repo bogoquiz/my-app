0872dbc2b6e490cd109dde8dfd6adab7311c09a3
Fredy B <tovar_fredy@hotmail.com>
2017-06-16 16:07:40 -0500

    form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+  	super();
+  	this.state = {
+  		name: "",
+  		terms: false
+  	}
+  }	
+
   render() {
     return (
-        <h1>HOLA REACT!!!</h1>
+        <div>
+        	<h1>Hola form</h1>
+        	<input type="text" value={this.state.name} onChange={this.updateName.bind(this)} />
+        	<div>
+        		<label>
+        			<input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)} />Acepto
+        		</label>
+        	</div>
+        	<button onClick={this.sayHi.bind(this)}>Say hi!</button>
+        </div>
     );
   }
+
+  updateName(event) {
+  	this.setState({
+  		name: event.target.value
+  	})
+  }
+
+  updateTerms(event){
+  	this.setState({
+  		terms: event.target.checked
+  	})
+  }
+
+  sayHi(){
+  	if (this.state.terms) {
+  		alert('hola ' + this.state.name)
+  	} else{
+  		alert('Debe aceptar ')
+  	};
+  	
+  }
+
 }
 
 export default App;
